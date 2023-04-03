e3e7b8b8679699202222b0c87e8cab3c608da601
NS-Snow <hichoco94116@gmail.com>
2023-04-03 15:48:09 +0800

    4/3

--- scripts/js.js
+++ scripts/js.js
@@ -1,10 +1,23 @@
-function toggle_info(){
+function toggle_info1(){
     const invisable = document.getElementById("introduce");
-    const invisable2 = document.getElementById("introduce2");
     invisable.classList.toggle("invisible");
+}
+function toggle_info2(){
+    const invisable2 = document.getElementById("introduce2");
     invisable2.classList.toggle("invisible");
 }
-
+function toggle_info3(){
+    const invisable2 = document.getElementById("introduce3");
+    invisable2.classList.toggle("invisible");
+}
+function toggle_info4(){
+    const invisable2 = document.getElementById("introduce4");
+    invisable2.classList.toggle("invisible");
+}
+function toggle_info6(){
+    const invisable2 = document.getElementById("introduce6");
+    invisable2.classList.toggle("invisible");
+}
 // 設定TIMER按鈕 開始暫停 清除
 
 var stopwatch=function(){
@@ -126,3 +139,73 @@ document.body.onkeyup=function(e){
     }
 
 }
+
+
+
+function updateTime() {
+    var time = new Date();
+    var dname = time.getDay(),
+        mo = time.getMonth(),
+        yr = time.getFullYear(),
+        dnum = time.getDate(),
+        hr = time.getHours(),
+        min = time.getMinutes(),
+        sec = time.getSeconds(),
+        pe = "AM";
+
+    // 抓AM或PM
+    if (hr >= 12) {
+        pe = "PM";
+    } else {
+        pe = "AM"
+    }
+
+    // 轉換成兩位數
+    Number.prototype.pad = function(digits) {
+        for (var n = this.toString(); n.length < digits; n = 0 + n);
+        return n;
+    }
+
+    var months = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+    var week = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
+    var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
+    var values = [week[dname], months[mo], dnum.pad(2), yr, hr.pad(2), min.pad(2), sec.pad(2), pe];
+
+    for (var i = 0; i < ids.length; i++)
+        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+
+}
+
+function startTime() {
+    updateTime();
+    window.setInterval("updateTime()", 1);
+}
+
+
+
+
+// 
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+// 
+
+function setClock() {
+    var currentDate = new Date()
+    var secondsRatio = currentDate.getSeconds() / 60
+    var minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    var hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+}
+
+// 
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setInterval(setClock, 1000)
+
+setClock()
